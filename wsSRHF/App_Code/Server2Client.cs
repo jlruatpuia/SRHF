c033647e7784693ea1665966a9fd490bbb09c5c4
jlruatpuia <jlruatpuia@gmail.com>
2016-07-18 22:39:23 +0530

    Project created

---
+++ wsSRHF/App_Code/Server2Client.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Server2Client
+/// </summary>
+public class Server2Client
+{
+    public int count { get; set; }
+    public double value { get; set; }
+    public string message { get; set; }
+    public DataTable dt { get; set; }
+    public DataSet ds { get; set; }
+}
+
+public class Patient
+{
+    public int ID { get; set; }
+    public string EMRNo { get; set; }
+    public string PName { get; set; }
+    public string Address { get; set; }
+    public string Sex { get; set; }
+    public int Age { get; set; }
+
+}
+
+public class OPD
+{
+    public int ID { get; set; }
+    public int DailyNo { get; set; }
+    public int MonthlyNo { get; set; }
+    public int YearlyNo { get; set; }
+    public string EMRNo { get; set; }
+    public int OPDNo { get; set; }
+
+}
+
+public class ECG
+{
+    public int ID { get; set; }
+    public int DailyNo { get; set; }
+    public int MonthlyNo { get; set; }
+    public string EMRNo { get; set; }
+    public string Diagnosis { get; set; }
+    public string ReceiptNo { get; set; }
+    public string Remarks { get; set; }
+    public double Charge { get; set; }
+}
+
+public class USG
+{
+    public int ID { get; set; }
+    public int DailyNo { get; set; }
+    public int MonthlyNo { get; set; }
+    public string EMRNo { get; set; }
+    public string Diagnosis { get; set; }
+    public string ReceiptNo { get; set; }
+    public string Remarks { get; set; }
+    public double Charge { get; set; }
+}
+
+public class XRAY
+{
+    public int ID { get; set; }
+    public int DailyNo { get; set; }
+    public int MonthlyNo { get; set; }
+    public string EMRNo { get; set; }
+    public string Diagnosis { get; set; }
+    public string ReceiptNo { get; set; }
+    public string Remarks { get; set; }
+    public double Charge { get; set; }
+}
+
+public class LAB
+{
+    public int ID { get; set; }
+    public int SlNo { get; set; }
+    public int DailyNo { get; set; }
+    public string ReceiptNo { get; set; }
+    public string EMRNo { get; set; }
+    public string PatientType { get; set; }
+    public string Ward { get; set; }
+    public string MRDNo { get; set; }
+    public string TestPerformed { get; set; }
+    public double Charge { get; set; }
+    public string Remarks { get; set; }
+
+}
+
+public class Casualty
+{
+    public int ID { get; set; }
+    public string RegNo { get; set; }
+    public int SlNo { get; set; }
+    public DateTime RegDate { get; set; }
+    public DateTime RegTime { get; set; }
+    public string PName { get; set; }
+    public string Address { get; set; }
+    public int Age { get; set; }
+    public string Sex { get; set; }
+    public string Diagnosis { get; set; }
+    public string Treatment { get; set; }
+
+}
+
+public class MRD
+{
+    public int ID { get; set; }
+    public int SlNo { get; set; }
+    public string EMRNo { get; set; }
+    public string Diagnosis { get; set; }
+    public string MRDNo { get; set; }
+    public string ICDCode { get; set; }
+}
---
+++ wsSRHF/App_Code/wsPatient.cs
@@ -0,0 +1,124 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for wsPatient
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class wsPatient : System.Web.Services.WebService
+{
+    MySqlConnection cm = new MySqlConnection(ConfigurationManager.ConnectionStrings["cs"].ConnectionString);
+
+    [WebMethod]
+    public Server2Client GetPatients()
+    {
+        Server2Client sc = new Server2Client();
+        MySqlCommand cmd = new MySqlCommand("SELECT ID, EMRNo, PName, Address, Age, Sex FROM patient", cm);
+        MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        sc.count = ds.Tables[0].Rows.Count;
+        sc.dt = ds.Tables[0];
+        return sc;
+    }
+
+    [WebMethod]
+    public Patient GetPatient(int ID)
+    {
+        Patient p = new Patient();
+        MySqlCommand cmd = new MySqlCommand("SELECT EMRNo, PName, Address, Age, Sex FROM patient WHERE ID=" + ID, cm);
+        try
+        {
+            cm.Open();
+            MySqlDataReader rd = cmd.ExecuteReader();
+            rd.Read();
+            p.EMRNo = rd[0].ToString();
+            p.PName = rd[1].ToString();
+            p.Address = rd[2].ToString();
+            p.Age = Convert.ToInt32(rd[3]);
+            p.Sex = rd[4].ToString();
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+        finally { cm.Close(); }
+        return p;
+    }
+
+    [WebMethod]
+    public Server2Client AddPatient(Patient p)
+    {
+        Server2Client sc = new Server2Client();
+        MySqlCommand cmd = new MySqlCommand("INSERT INTO patient (EMRNo, PName, Address, Age, Sex) VALUES (@EMR, @PNM, @ADR, @AGE, @SEX)", cm);
+        cmd.Parameters.AddWithValue("@EMR", p.EMRNo);
+        cmd.Parameters.AddWithValue("@PNM", p.PName);
+        cmd.Parameters.AddWithValue("@ADR", p.Address);
+        cmd.Parameters.AddWithValue("@AGE", p.Age);
+        cmd.Parameters.AddWithValue("@SEX", p.Sex);
+
+        try
+        {
+            cm.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch(Exception ex)
+        {
+            sc.message = ex.Message;
+        }
+        finally { cm.Close(); }
+        return sc;
+    }
+
+    [WebMethod]
+    public Server2Client UpdatePatient(Patient p)
+    {
+        Server2Client sc = new Server2Client();
+        MySqlCommand cmd = new MySqlCommand("UPDATE patient SET EMRNo=@EMR, PName=@PNM, Address=@ADR, Age=@AGE, Sex=@SEX WHERE ID=" + p.ID, cm);
+        cmd.Parameters.AddWithValue("@EMR", p.EMRNo);
+        cmd.Parameters.AddWithValue("@PNM", p.PName);
+        cmd.Parameters.AddWithValue("@ADR", p.Address);
+        cmd.Parameters.AddWithValue("@AGE", p.Age);
+        cmd.Parameters.AddWithValue("@SEX", p.Sex);
+
+        try
+        {
+            cm.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            sc.message = ex.Message;
+        }
+        finally { cm.Close(); }
+        return sc;
+    }
+
+    [WebMethod]
+    public Server2Client DeletePatient(Patient p)
+    {
+        Server2Client sc = new Server2Client();
+        MySqlCommand cmd = new MySqlCommand("DELETE FROM patient WHERE ID=" + p.ID, cm);
+        
+        try
+        {
+            cm.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            sc.message = ex.Message;
+        }
+        finally { cm.Close(); }
+        return sc;
+    }
+}
